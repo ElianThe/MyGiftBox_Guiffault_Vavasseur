a9ea5a0ebd1fbc53b66bae8ef58888df4960a5c8
Guiffault Elian <guiffaul2u@etu.univ-lorraine.fr>
2023-05-09 22:25:29 +0200

    bootstrap, index.html et routes ont été modifié et fonctionnel. On vient de créer le dispatcher. routes permet de définir les routes choisis. Bootstrap permet de configurer et d'initier notre application.

--- gift/gift.appli/src/conf/routes.php
+++ gift/gift.appli/src/conf/routes.php
@@ -1,1 +1,26 @@
 <?php
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+
+return function (Slim\App $app) {
+    $app->get('/',
+        function (Request $rq, Response $rs, $args) : Response{
+            $html = <<<EOM
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+                <html lang="en">
+                <head>
+                    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+                    <title>Document</title>
+                </head>
+                <body>
+                <p>Coucouc</p>
+                </body>
+                </html>
+EOM;
+
+            $rs->getBody()->write($html);
+            return $rs;
+        });
+        };
