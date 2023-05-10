e75ef14fe73febe332ee9b5e661fe932b65b68b6
Regis <vavasseu2u@etu.univ-lorraine.fr>
2023-05-10 16:39:17 +0200

    Connection à la bd de Eloquent

--- gift/gift.appli/src/services/utils/Eloquent.php
+++ gift/gift.appli/src/services/utils/Eloquent.php
@@ -8,7 +8,7 @@ class Eloquent {
 
     public static function init ($filename) {
         $db = new DB();
-        $db->addConnection(parse_ini_string($filename));
+        $db->addConnection(parse_ini_file($filename));
         $db->bootEloquent();
         $db->setAsGlobal();
     }
