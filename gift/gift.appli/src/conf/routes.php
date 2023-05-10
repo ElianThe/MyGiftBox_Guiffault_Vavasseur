778959cf1cf9349193659d2f1e4f90f3218b5627
Regis <vavasseu2u@etu.univ-lorraine.fr>
2023-05-10 08:14:03 +0200

    ajout des 3 premieres routes

--- gift/gift.appli/src/conf/routes.php
+++ gift/gift.appli/src/conf/routes.php
@@ -1,1 +1,97 @@
 <?php
+
+use gift\app\models\Categorie;
+use gift\app\models\Prestation;
+use Slim\Psr7\Request;
+use Slim\Psr7\Response;
+
+$app = \Slim\Factory\AppFactory::create();
+
+// Route 1 : liste des catégories
+$app->get('/categories', function (Request $request, Response $response, array $args) {
+    $categories = Categorie::all();
+    $html = <<<HTML
+        <!DOCTYPE html>
+        <html lang="fr">
+        <head>
+            <meta charset="UTF-8">
+            <title>Liste des catégories</title>
+        </head>
+        <body>
+            <h1>Liste des catégories</h1>
+            <ul>
+    HTML;
+    foreach ($categories as $categorie) {
+        $html .= <<<HTML
+                <li><a href='/categorie/{$categorie->id}'>{$categorie->id} - {$categorie->libelle}</a></li>
+            </ul>
+        </body> 
+        </html>
+    HTML;
+    }
+
+    $response->getBody()->write($html);
+    return $response;
+});
+
+// Route 2 : Affichage d'une catégorie
+$app->get('/categorie/{id}', function (Request $request, Response $response, array $args) {
+    $categorie = Categorie::find($args['id']);
+    $html = <<<HTML
+        <!DOCTYPE html>
+        <html lang="fr">
+        <head>
+            <meta charset="UTF-8">
+            <title>Catégorie {$categorie->id}</title>
+        </head>
+        <body>
+            <h1>Catégorie {$categorie->id}</h1>
+            <ul>
+                <li>{$categorie->id} - {$categorie->libelle}</li>
+            </ul>
+        </body>
+        </html>
+    HTML;
+
+    $response->getBody()->write($html);
+    return $response;
+});
+
+// Route 3 : Affichage d'une prestation si l'ID est présent en paramètre
+$app->get('/prestation', function (Request $request, Response $response, array $args) {
+    $id = $request->getQueryParams()['id'] ?? null;
+    $html = null;
+    if ($id === null) {
+        $response->withStatus(400, "Paramètre absent");
+    } else {
+        $prestation = Prestation::find($id);
+        $html = <<<HTML
+            <!DOCTYPE html>
+            <html lang="fr">
+            <head>
+                <meta charset="UTF-8">
+                <title>Prestation</title>
+            </head>
+            <body>
+                <h1>Prestation : {$prestation->libelle} </h1>
+                <ul>
+                    <li>ID : {$prestation->id}</li>
+                    <li>Libellé : {$prestation->libelle}</li>
+                    <li>Description : {$prestation->description}</li>
+                    <li>Contenu : {$prestation->unite}</li>
+                    <li>Tarif : {$prestation->tarif}</li>
+                </ul>
+            </body>
+            </html>
+    HTML;
+    }
+    $response->getBody()->write($html);
+    return $response;
+
+
+
+
+
+
+
+
