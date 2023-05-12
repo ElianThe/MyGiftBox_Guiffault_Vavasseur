d4b12ad4e1725bb763991e486c69451c250b68f0
Guiffault Elian <guiffaul2u@etu.univ-lorraine.fr>
2023-05-12 11:03:06 +0200

    modification de CategoriesAction.php pour que ça me retourne des arrays au lieu d'une collection

--- gift/gift.appli/src/services/prestations/PrestationsService.php
+++ gift/gift.appli/src/services/prestations/PrestationsService.php
@@ -7,16 +7,16 @@ use gift\app\models\Prestation;
 
 class PrestationsService
 {
-    public function getCategories() {
-        return Categorie::get();
+    public function getCategories(): array {
+        return Categorie::get()->toArray();
     }
     public function getCategorieById(int $id): array {
-        return Categorie::where('id', $id)->first();
+        return Categorie::where('id', $id)->first()->toArray();
     }
     public function getPrestationById(string $id): array {
-        return Prestation::where('id', $id)->first();
+        return Prestation::where('id', $id)->first()->toArray();
     }
     public function getPrestationsbyCategorie(int $categ_id):array {
-        return Prestation::categorie()->where('id', $id);
+        return Prestation::categorie()->where('id', $id)->get()->toArray();
     }
 }
