79279c3d6c6112f11f7e1c8b26f0eaf809c2d4f5
Regis <vavasseu2u@etu.univ-lorraine.fr>
2023-05-19 09:58:04 +0200

    ajout du css

--- gift/gift.appli/src/actions/NewBoxesPostAction.php
+++ gift/gift.appli/src/actions/NewBoxesPostAction.php
@@ -13,11 +13,7 @@ class NewBoxesPostAction
         $params = $request->getParsedBody();
         $name = $params['name'] ?? '';
         $description = $params['description'] ?? '';
-        $html = <<<HTML
-                <h1>Nouvelle box créée</h1>
-                <p>Nom : {$name}</p>
-                <p>Description : {$description}</p>
-            HTML;
+
         $box = new Box();
         $box->libelle = $name;
         $box->description = $description;
