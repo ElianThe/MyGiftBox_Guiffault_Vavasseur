ce4f90116e0d12653b297ff3e1ee4bba07d9b1e3
Regis <vavasseu2u@etu.univ-lorraine.fr>
2023-05-26 11:28:46 +0200

    ajout du service pour la gestion de token

---
+++ gift/app/services/utils/CsrfService.php
@@ -0,0 +1,27 @@
+<?php
+
+class CsrfService
+{
+
+    /**
+     * @throws Exception
+     */
+    public static function generateToken()
+    {
+        $token = bin2hex(random_bytes(32));
+        $_SESSION['csrf_token'] = $token;
+        return $token;
+    }
+
+    public static function checkToken($token)
+    {
+        if (isset($_SESSION['csrf_token']) && $token === $_SESSION['csrf_token']) {
+            unset($_SESSION['csrf_token']);
+            return true;
+        } else {
+            unset($_SESSION['csrf_token']);
+            throw new Exception('CSRF token mismatch');
+        }
+    }
+
+}
