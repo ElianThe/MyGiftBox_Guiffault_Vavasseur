cfd4be355219bfeec85c0957beeffa3ff38573d5
Regis <vavasseu2u@etu.univ-lorraine.fr>
2023-06-01 14:55:14 +0200

    Ajout des status des boxs

--- gift/gift.appli/src/models/Box.php
+++ gift/gift.appli/src/models/Box.php
@@ -10,6 +10,13 @@ class Box extends Model
     protected $primaryKey = 'id';
     public $timestamps = true;
 
+    const CREATED = 1;
+    const NOT_CREATED = 0;
+    const STATUT = [
+        self::CREATED => 'Créée',
+        self::NOT_CREATED => 'Non créée',
+    ];
+
     public function prestations () {
         return $this->belongsToMany('gift\app\models\Prestation', 'box2presta', 'box_id', 'presta_id')->withPivot(['quantite']);
     }
