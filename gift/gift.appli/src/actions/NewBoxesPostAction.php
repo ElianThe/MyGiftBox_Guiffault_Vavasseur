d7ce041c2cfd9bc72dde91ad7e17958675aa91a4
Guiffault Elian <guiffaul2u@etu.univ-lorraine.fr>
2023-06-01 15:32:47 +0200

    ajout du l'action de la route post boxes/new

--- gift/gift.appli/src/actions/NewBoxesPostAction.php
+++ gift/gift.appli/src/actions/NewBoxesPostAction.php
@@ -2,24 +2,30 @@
 
 namespace gift\app\actions;
 
-use gift\app\models\Box;
+use gift\app\services\box\BoxService;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Slim\Routing\RouteContext;
+use Slim\Views\Twig;
 
 class NewBoxesPostAction
 {
     function __invoke(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
     {
         $params = $request->getParsedBody();
-        $name = $params['name'] ?? '';
-        $description = $params['description'] ?? '';
 
-        $box = new Box();
-        $box->libelle = $name;
-        $box->description = $description;
+        $data = [
+        'libelle' =>  $params['libelle'],
+        'description' => $params['description'] ?? '',
+        'kdo' => $params['kdo'],
+        'message_kdo' => $params['message_kdo'],
+        ];
 
-        $box->save();
-        $response->getBody()->write($html);
-        return $response;
+        $box = new BoxService();
+        $newBox = $box->addBox($data);
+
+        $routeParser = RouteContext::fromRequest($request)->getRouteParser();
+        $url = $routeParser->urlFor('categories');
+        return $response->withHeader('Location', $url)->withStatus(302);
     }
 }
